74c2b915e35a2789a5488d349b21ebe14bed1e54
Vanessa Vaegas <vanessa.vargas4f@gmail.com>
2022-10-02 01:31:48 -0300

    update style header

--- src/pages/Home/components/Header.jsx
+++ src/pages/Home/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import ReactAudioPlayer from 'react-audio-player';
 
 export default function Header() {
     return (
-      <div className="flex-container menu">
+      <div className="flex-container menu header">
         <div><h1>Amor ao Café</h1></div>
-        <audio src="assets/vitao-cafe.mp3" autoplay loop controls>
+        <ReactAudioPlayer
+          src="assets/vitao-cafe.mp3"
+          autoPlay loop controls>
           Seu Browser não tem suporte para a execução de áudio
-        </audio>
+        </ReactAudioPlayer>
+        
         <ul className="list-items">
           <li><a href="#tipos-cafe">Tipos</a></li>
           <li><a href="#metodos">Preparo</a></li>
