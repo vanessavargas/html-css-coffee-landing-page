f45b6ce6bb3ace9c8e2a53633fae0fd3e3f24dea
Vanessa Vaegas <vanessa.vargas4f@gmail.com>
2022-10-05 21:04:31 -0300

    update header

--- src/pages/Home/components/Header.jsx
+++ src/pages/Home/components/Header.jsx
@@ -5,11 +5,13 @@ export default function Header() {
     return (
       <div className="flex-container menu header">
         <div><h1>Amor ao Café</h1></div>
+        {/* 
         <ReactAudioPlayer
           src="assets/vitao-cafe.mp3"
           autoPlay loop controls>
           Seu Browser não tem suporte para a execução de áudio
         </ReactAudioPlayer>
+        */}
         
         <ul className="list-items">
           <li><a href="#tipos-cafe">Tipos</a></li>
